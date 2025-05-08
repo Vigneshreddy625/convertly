957aec11c56df688208709c05c4d2fad7a4fcbdb
Vigneshreddy <vigneshreddy625@gmail.com>
2025-05-08 18:53:31 +0530

    made changes

--- Backend/app/api/routes/auth/__init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.api.routes.auth import register, token, user_info, login, logout
-
-router = APIRouter(prefix="/auth", tags=["authentication"])
-router.include_router(register.router)
-router.include_router(token.router)
-router.include_router(user_info.router)
-router.include_router(login.router)
-router.include_router(logout.router)
